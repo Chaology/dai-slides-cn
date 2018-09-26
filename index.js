2d590b5302f9b96391b620556a16c9d400399080
Chao Pan <chaopaninchina@gmail.com>
2018-09-26 11:26:32 +0800

    First slides version

---
+++ index.js
@@ -0,0 +1,9 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Presentation from './presentation';
+import PropTypes from "prop-types";
+import { AppContainer } from "react-hot-loader";
+import Redbox from "redbox-react";
+
+ReactDOM.render(<Presentation />, document.getElementById('root'));
